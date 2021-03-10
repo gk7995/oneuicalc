33d6ea8798f391003f5026dc298a94688400fb84
gk7995 <ghkhatri7@gmail.com>
2021-03-10 17:49:01 +0530

    34th

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
+      navigator.serviceWorker.register("sw.js")
   }
