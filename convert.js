2ac6955fcbee2ca97a1d97dd203f54f1ea3720f9
gk7995 <ghkhatri7@gmail.com>
2022-04-04 13:35:01 +0530

    update convert.js

--- convert.js
+++ convert.js
@@ -621,18 +621,18 @@ function CurrConv(source, valnum8) {
         eur.value = (valnum8 * 0.012).toFixed(3);
     }
     if (source == "dol") {
-        rup.value = (valnum8 * 75.88).toFixed(2);
+        rup.value = (valnum8 * 75.42).toFixed(2);
         pou.value = (valnum8 * 0.76).toFixed(2);
-        eur.value = (valnum8 * 0.90).toFixed(2);
+        eur.value = (valnum8 * 0.91).toFixed(2);
     }
     if (source == "pou") {
-        rup.value = (valnum8 * 99.60).toFixed(2);
+        rup.value = (valnum8 * 99.01).toFixed(2);
         dol.value = (valnum8 * 1.31).toFixed(2);
-        eur.value = (valnum8 * 1.18).toFixed(2);
+        eur.value = (valnum8 * 1.19).toFixed(2);
     }
     if (source == "eur") {
-        rup.value = (valnum8 * 84.45).toFixed(2);
-        dol.value = (valnum8 * 1.11).toFixed(2);
-        pou.value = (valnum8 * 0.85).toFixed(2);
+        rup.value = (valnum8 * 83.21).toFixed(2);
+        dol.value = (valnum8 * 1.10).toFixed(2);
+        pou.value = (valnum8 * 0.84).toFixed(2);
     }
 }
