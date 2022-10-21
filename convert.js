5702fcb249f0388d7da133a04e05ece76998754f
gk7995 <ghkhatri7@gmail.com>
2022-10-21 17:17:43 +0530

    change 1

--- convert.js
+++ convert.js
@@ -618,21 +618,21 @@ function CurrConv(source, valnum8) {
     if (source == "rup") {
         dol.value = (valnum8 * 0.012).toFixed(3);
         pou.value = (valnum8 * 0.011).toFixed(4);
-        eur.value = (valnum8 * 0.013).toFixed(3);
+        eur.value = (valnum8 * 0.012).toFixed(3);
     }
     if (source == "dol") {
-        rup.value = (valnum8 * 82.93).toFixed(2);
-        pou.value = (valnum8 * 0.89).toFixed(2);
-        eur.value = (valnum8 * 1.02).toFixed(2);
+        rup.value = (valnum8 * 82.90).toFixed(2);
+        pou.value = (valnum8 * 0.90).toFixed(2);
+        eur.value = (valnum8 * 1.03).toFixed(2);
     }
     if (source == "pou") {
-        rup.value = (valnum8 * 93.34).toFixed(2);
-        dol.value = (valnum8 * 1.13).toFixed(2);
-        eur.value = (valnum8 * 1.15).toFixed(2);
+        rup.value = (valnum8 * 92.13).toFixed(2);
+        dol.value = (valnum8 * 1.11).toFixed(2);
+        eur.value = (valnum8 * 1.14).toFixed(2);
     }
     if (source == "eur") {
-        rup.value = (valnum8 * 81.24).toFixed(2);
-        dol.value = (valnum8 * 0.98).toFixed(2);
-        pou.value = (valnum8 * 0.87).toFixed(2);
+        rup.value = (valnum8 * 80.80).toFixed(2);
+        dol.value = (valnum8 * 0.97).toFixed(2);
+        pou.value = (valnum8 * 0.88).toFixed(2);
     }
 }
